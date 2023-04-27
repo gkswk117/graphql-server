84dad07b973bdd87293f7182c528047484472c82
gkswk117 <gkswk117@gmail.com>
2023-04-27 14:11:28 +0900

    [GraphQL로 영화 API 만들기] #4.4 Non Nullable Fields (07:25) 완료

--- server.js
+++ server.js
@@ -2,21 +2,23 @@ import { ApolloServer, gql } from "apollo-server"
 
 const typeDefs = gql`
     type User{
-        id: ID
-        username: String
+        id: ID!
+        username: String!
+        firstName: String!
+        lastName: String
     }
     type Tweet{
-        id: ID
-        text: String
-        author: User
+        id: ID!
+        text: String!
+        author: User!
     }
     type Query {
-        allTweets: [Tweet]
-        tweet(id: ID): Tweet
+        allTweets: [Tweet!]!
+        tweet(id: ID!): Tweet
     }    
     type Mutation{
-        postTweet(text: String, userId: ID): Tweet
-        deleteTweet(id: ID): Boolean
+        postTweet(text: String!, userId: ID!): Tweet!
+        deleteTweet(id: ID!): Boolean!
     }
 `
 //sdl(schema definition language)
