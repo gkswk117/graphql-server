0c8afe4e99b7d8a4bb71fc1b480f34bf124da1d3
gkswk117 <gkswk117@gmail.com>
2023-04-27 16:09:14 +0900

    [GraphQL로 영화 API 만들기]  #4.7 Mutation Resolvers (09:11) 완료

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { ApolloServer, gql } from "apollo-server"
 
-const fakeDatabase = [
+let fakeDatabase = [
     {
         id:"1",
         text:"first Tweet"
@@ -41,19 +41,19 @@ const typeDefs = gql`
 //Mutation type은 Post request url을 열어주는 것이다.
 //
 
-// const resolvers = {
-//     Query:{
-//         tweet(){
-//             console.log("I'm called.")
-//             return null
-//         },
-//         ping(){
-//             console.log("ping is called.")
-//             return "pong"
-//         }
-//     }
-// }
-
+const resolvers1 = {
+    Query:{
+        tweet(){
+            console.log("I'm called.")
+            return null
+        },
+        ping(){
+            console.log("ping is called.")
+            return "pong"
+        }
+    }
+}
+let count = 0
 const resolvers = {
     Query:{
         allTweets(){
@@ -63,9 +63,33 @@ const resolvers = {
             const id = argument.id
             console.log(argument, id)
             return fakeDatabase.find((tweet)=>tweet.id === id)
+            //자바스크립트 배열 find 함수
+        }
+    },
+    Mutation:{
+        postTweet(_,argument){
+            const id = argument.id
+            const text = argument.text
+            const newTweet = {
+                id: fakeDatabase.length+1,
+                text:text
+            }
+            count = count+1
+            fakeDatabase.push(newTweet)
+            return fakeDatabase[count]
+        },
+        deleteTweet(_,argument){
+            const id = argument.id
+            const tweet = fakeDatabase.find(tweet => tweet.id === id)
+            if(!tweet) return false
+            fakeDatabase= fakeDatabase.filter(tweet => tweet.id!==id)
+            //자바스크립트 배열 filter 함수
+            return true
         }
     }
 }
+//이렇게 Query와 Mutation으로 나누는 이유는 그냥 코딩하기 편하려고.
+//구분을 지어줌으로써 개발자가 무언가 수정하는건 Mutation에서 찾고, 검색하는 건 Query에서 찾고
 
 const server = new ApolloServer({typeDefs, resolvers})
 //resolvers 자리에 다른 이름을 넣으면 오류가 난다. 왜 그럴까?
